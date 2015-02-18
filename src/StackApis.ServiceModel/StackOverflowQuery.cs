274c9b75446fa5c8bdb0212074e5caf978393db8
Demis Bellot <demis.bellot@gmail.com>
2015-02-17 22:33:32 -0500

    Upgrade to v4.0.37

--- src/StackApis.ServiceModel/StackOverflowQuery.cs
+++ src/StackApis.ServiceModel/StackOverflowQuery.cs
@@ -4,6 +4,9 @@ using StackApis.ServiceModel.Types;
 namespace StackApis.ServiceModel
 {
     [Route("/questions")]
+    [AutoQueryViewer(
+        Title = "Search StackOverflow Questions", Description = "Search for ServiceStack Questions on StackOverflow", IconUrl = "/Content/app/stacks-white-75.png",
+        DefaultSearchField = "Title", DefaultSearchType = "Contains", DefaultSearchText = "ServiceStack")]
     public class StackOverflowQuery : QueryBase<Question>
     {
         public int? ScoreGreaterThan { get; set; }
